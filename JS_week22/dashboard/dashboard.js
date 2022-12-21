c4752b3b6f5ab6b8d688a28533dbe98ffb99e3fb
Ольга Малышева <olga.maysheva.smm@gmail.com>
2022-12-21 15:18:22 +0200

    ex 2

---
+++ JS_week22/dashboard/dashboard.js
@@ -0,0 +1,30 @@
+const Chart = require('chart.js');
+
+const ctxFirst = document.getElementById('myChart-1').getContext('2d');
+
+let dataJsonFirst = `{
+    "labels": ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul"],
+    "label": "title",
+    "data": [133, 198, 320, 591, 520, 354, 632]
+}`
+let objDataFirst = JSON.parse(dataJsonFirst);
+
+const myChartFirst = new Chart(ctxFirst, {
+    type: 'line',
+
+    data: {
+        labels: objDataFirst.labels,
+        datasets: [{
+            label: objDataFirst.label,
+            data: objDataFirst.data,
+            fill: 'origin',
+            backgroundColor: [
+                'rgba(8, 69, 166, 0.8)'],
+            borderColor: [
+                'rgba(23, 131, 241, 1)',
+            ],
+            tension: 0.4,
+            borderWidth: 2
+        }]
+    }
+});
