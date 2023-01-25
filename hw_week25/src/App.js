d6052e3f3b2e524ff725d4349f880d3df901a1fc
Ольга Малышева <olga.maysheva.smm@gmail.com>
2023-01-25 18:57:33 +0200

    knb

--- hw_week25/src/App.js
+++ hw_week25/src/App.js
@@ -15,6 +15,43 @@ export const options = {
   title: "My Daily Activities",
   is3D: true,
 };
+export const data1 = [
+  ["Language", "Speakers (in millions)"],
+  ["Assamese", 13],
+  ["Bengali", 83],
+  ["Bodo", 1.4],
+  ["Dogri", 2.3],
+  ["Gujarati", 46],
+  ["Hindi", 300],
+  ["Kannada", 38],
+  ["Kashmiri", 5.5],
+  ["Konkani", 5],
+  ["Maithili", 20],
+  ["Malayalam", 33],
+  ["Manipuri", 1.5],
+  ["Marathi", 72],
+  ["Nepali", 2.9],
+  ["Oriya", 33],
+  ["Punjabi", 29],
+  ["Sanskrit", 0.01],
+  ["Santhali", 6.5],
+  ["Sindhi", 2.5],
+  ["Tamil", 61],
+  ["Telugu", 74],
+  ["Urdu", 52],
+];
+
+export const options1 = {
+  title: "Indian Language Use",
+  legend: "none",
+  pieSliceText: "label",
+  slices: {
+    4: { offset: 0.2 },
+    12: { offset: 0.3 },
+    14: { offset: 0.4 },
+    15: { offset: 0.5 },
+  },
+};
 function App() {
   return (
     <div className="App">
@@ -23,6 +60,13 @@ function App() {
       chartType="PieChart"
       data={data}
       options={options}
+      width={"100%"}
+      height={"400px"}
+    />
+    <Chart
+      chartType="PieChart"
+      data={data1}
+      options={options1}
       width={"100%"}
       height={"400px"}
     />
